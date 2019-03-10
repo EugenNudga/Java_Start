8eb902b29d9f5a15c03ced252535aeb19c1c00bd
EugenNudga <evgeniybond@gmail.com>
2019-03-10 11:47:32 +0100

    Added new classes in Homework11 and Homework13

--- src/main/java/homework13/Main.java
+++ src/main/java/homework13/Main.java
@@ -3,21 +3,19 @@ package homework13;
 import java.util.Scanner;
 
 public class Main {
+    // main and logic should be in separate classes
     public static void main(String[] args) {
         /**
          * Возвести число в квадрат, куб, степень. Важно: не использовать Math.pow().
          */
-        
+        //Method  pow doesn't have modifier static. You should create object of class BaseNumberAndItsPower and use it.
         //Hello from Github
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a base number:");
-        int number = scanner.nextInt();
-        System.out.println("Please enter an exponent (or a power) number:");
-        int power = scanner.nextInt();
-        int result = 0;
-        for (int i = 0; i < power; i++) {
-            result += number;
-        }
-        System.out.println("The result is: " + result);
+        double number = scanner.nextDouble();
+        System.out.println("Please enter an exponent number:");
+        int exponent = scanner.nextInt();
+        BaseNumberAndItsPower baseNumberAndItsPower = new BaseNumberAndItsPower();
+        System.out.println("The result is: " + baseNumberAndItsPower.pow(number,exponent));
     }
 }
